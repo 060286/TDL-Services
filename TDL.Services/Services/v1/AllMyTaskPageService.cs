bd4f01e20a1919d8ff780496039b1b41c4db7b3a
tamlv <tamle.dev@gmail.com>
2023-04-02 10:55:17 +0700

    Created remind at api and update response

--- TDL.Services/Services/v1/AllMyTaskPageService.cs
+++ TDL.Services/Services/v1/AllMyTaskPageService.cs
@@ -6,6 +6,7 @@ using TDL.Domain.Entities;
 using TDL.Infrastructure.Persistence.Repositories.Repositories;
 using TDL.Infrastructure.Persistence.UnitOfWork.Interfaces;
 using TDL.Services.Dto.AllMyTask;
+using TDL.Services.Dto.MyDayPage;
 using TDL.Services.Services.v1.Interfaces;
 
 namespace TDL.Services.Services.v1
@@ -15,14 +16,17 @@ namespace TDL.Services.Services.v1
         private readonly IRepository<Todo> _todoRepository;
         private readonly IUnitOfWorkProvider _uow;
         private readonly IMapper _mapper;
+        private readonly IColorService _colorService;
 
         public AllMyTaskPageService(IRepository<Todo> todoRepository, 
             IUnitOfWorkProvider uow,
-            IMapper mapper)
+            IMapper mapper,
+            IColorService colorService)
         {
             _todoRepository = todoRepository;
             _uow = uow;
             _mapper = mapper;
+            _colorService = colorService;
         }
 
         public AllMyTaskListResponseDto GetAllTask(DateTime datetime)
@@ -36,18 +40,78 @@ namespace TDL.Services.Services.v1
 
             var todayTask = _todoRepository.GetAll(true)
                 .Where(td => td.TodoDate.Date == datetime.Date)
-                .Select(x => AllTaskItemResponseDto.FromTodo(x))
+                .Select(td => new AllTaskItemResponseDto()
+                {
+                    Id = td.Id,
+                    CategoryName = td.TodoCategory.Title,
+                    Title = td.Title,
+                    TodoDate = td.TodoDate,
+                    Description = td.Description,
+                    IsCompleted = td.IsCompleted,
+                    Priority = td.Priority,
+                    RemindedAt = td.RemindedAt,
+                    Tag = _colorService.PriorityColor(td.Tag),
+                    FileName = td.FileName,
+                    Status = td.Status,
+                    IsArchieved = td.IsArchieved,
+                    SubTasks = td.SubTasks.Select(st => new SubTaskResponse()
+                    {
+                        IsCompleted = st.IsCompleted,
+                        Id = st.Id,
+                        Name = st.Title
+                    }).ToList(),
+                })
                 .ToList();
 
             var tomorrowTask = _todoRepository.GetAll(true)
                 .Where(td => td.TodoDate.Date == nextDay.Date)
-                .Select(x => AllTaskItemResponseDto.FromTodo(x))
+                .Select(td => new AllTaskItemResponseDto()
+                {
+                    Id = td.Id,
+                    CategoryName = td.TodoCategory.Title,
+                    Title = td.Title,
+                    TodoDate = td.TodoDate,
+                    Description = td.Description,
+                    IsCompleted = td.IsCompleted,
+                    Priority = td.Priority,
+                    RemindedAt = td.RemindedAt,
+                    Tag = _colorService.PriorityColor(td.Tag),
+                    FileName = td.FileName,
+                    Status = td.Status,
+                    IsArchieved = td.IsArchieved,
+                    SubTasks = td.SubTasks.Select(st => new SubTaskResponse()
+                    {
+                        IsCompleted = st.IsCompleted,
+                        Id = st.Id,
+                        Name = st.Title
+                    }).ToList(),
+                })
                 .ToList();
 
             var upCommingDayTask = _todoRepository.GetAll(true)
                 .Where(td => td.TodoDate.Date > firstUpcomingDay.Date && 
                              td.TodoDate.Date < lastUpcomingDay.Date)
-                .Select(x => AllTaskItemResponseDto.FromTodo(x))
+                .Select(td => new AllTaskItemResponseDto()
+                {
+                    Id = td.Id,
+                    CategoryName = td.TodoCategory.Title,
+                    Title = td.Title,
+                    TodoDate = td.TodoDate,
+                    Description = td.Description,
+                    IsCompleted = td.IsCompleted,
+                    Priority = td.Priority,
+                    RemindedAt = td.RemindedAt,
+                    Tag = _colorService.PriorityColor(td.Tag),
+                    FileName = td.FileName,
+                    Status = td.Status,
+                    IsArchieved = td.IsArchieved,
+                    SubTasks = td.SubTasks.Select(st => new SubTaskResponse()
+                    {
+                        IsCompleted = st.IsCompleted,
+                        Id = st.Id,
+                        Name = st.Title
+                    }).ToList(),
+                })
                 .ToList();
 
             return new AllMyTaskListResponseDto
